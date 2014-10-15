7a62b92006d91d5a000719d255140583ba326deb
Julius Gonsior <julius@gaenseri.ch>
2014-10-15 23:01:19 +0200

    improved code

--- crawler.py
+++ crawler.py
@@ -1,10 +1,17 @@
 import urllib.request
 import re
 
-response = urllib.request.urlopen('http://reddit.com/')
-html = response.read().decode(response.headers.get_content_charset())
 
+def openUrlAndReturnContent(url):
+    response = urllib.request.urlopen(url)
+    return response.read().decode(response.headers.get_content_charset())
+
+frontPage = openUrlAndReturnContent("http://reddit.com/")
 #alle links zu unterseiten extrahieren
 topicsPattern = re.compile('http://www.reddit.com/r/\w*/comments/\w*/\w*/')
-for topicUrl in re.findall(topicsPattern, html):
+for topicUrl in re.findall(topicsPattern, frontPage):
     print("Found: " + topicUrl)
+
+
+
+
