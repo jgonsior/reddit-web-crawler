c476c01104ebe29338202e733eeae6aac5105e0f
Julius Gonsior <julius@gaenseri.ch>
2014-10-15 23:14:36 +0200

    iCrawling of kbkbkbkbkbkbkbkbkbkbkbkbkbkbkbUser fectkbkbtching worksfixes #ku@7I'm now using Requests instead of urllib - itskb's much faster :-)kd2:x:w:x:x:q!

--- crawler.py
+++ crawler.py
@@ -1,17 +1,22 @@
-import urllib.request
+import requests
 import re
 
 
 def openUrlAndReturnContent(url):
-    response = urllib.request.urlopen(url)
-    return response.read().decode(response.headers.get_content_charset())
+    response = requests.get(url)
+    return response.text
 
 frontPage = openUrlAndReturnContent("http://reddit.com/")
+
 #alle links zu unterseiten extrahieren
 topicsPattern = re.compile('http://www.reddit.com/r/\w*/comments/\w*/\w*/')
 for topicUrl in re.findall(topicsPattern, frontPage):
     print("Found: " + topicUrl)
 
+    #open topic
+    topicHtml = openUrlAndReturnContent(topicUrl)
 
-
+    userPattern = re.compile('http://www.reddit.com/user/\w*')
+    for user in re.findall(userPattern, topicHtml):
+        print("\tuser: " + user)
 
