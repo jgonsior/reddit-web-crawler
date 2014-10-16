68233fee408d55d7ef718804525a23fd6f80b197
Julius Gonsior <julius@gaenseri.ch>
2014-10-16 22:46:22 +0200

    Added two classes to store the extracted Topic's and Users, but haven't decided yet how to represent the graph connection between them best with sets and so one.
    Fixes #3

--- crawler.py
+++ crawler.py
@@ -2,21 +2,48 @@ import requests
 import re
 
 
+class Topic:
+    def __init__(self, name):
+        self.name = name
+        self.users = set()
+
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.commentedTopics = set()
+        self.createdTopics = set()
+
+    def printTopics(self):
+        for topic in self.commentedTopics:
+            print("\ttopic: " + topic.name)
+
 def openUrlAndReturnContent(url):
     response = requests.get(url)
     return response.text
 
 frontPage = openUrlAndReturnContent("http://reddit.com/")
 
-#alle links zu unterseiten extrahieren
+userSet = set()
+topicSet = set()
+
 topicsPattern = re.compile('http://www.reddit.com/r/\w*/comments/\w*/\w*/')
 for topicUrl in re.findall(topicsPattern, frontPage):
+    topic = Topic(topicUrl)
+    topicSet.add(topic)
+
     print("Found: " + topicUrl)
 
-    #open topic
     topicHtml = openUrlAndReturnContent(topicUrl)
 
-    userPattern = re.compile('http://www.reddit.com/user/\w*')
-    for user in re.findall(userPattern, topicHtml):
-        print("\tuser: " + user)
+    userPattern = re.compile('http://www.reddit.com/user/(\w+)')
+    for userName in re.findall(userPattern, topicHtml):
+        user = User(userName)
+        userSet.add(user)
+        topic.users.add(user)
+        user.commentedTopics.add(topic)
+        #print("\tuser: " + userName)
 
+for user in userSet :
+    print("user: " + user.name)
+    user.printTopics()
